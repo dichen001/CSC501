6ffa186153c92fd0c11f1790ab9cdaee5d8f0b26
Di (Jack) Chen <dchen20@ncsu.edu>
2015-10-14 01:46:28 -0400

    Revert "Upload the original XINU and PA0-1"
    This reverts commit 02848bc677ffe6b15ceba5a21c178fb71694ab4c.

--- original/lib/libxc/abs.c
+++
@@ -1,7 +0,0 @@
-abs(int arg)
-{
-
-	if(arg < 0)
-		arg = -arg;
-	return(arg);
-}
--- original/lib/libxc/strchr.c
+++
@@ -1,8 +0,0 @@
-/* Returns pointer to character, or NULL if not found */
-char *strchr (const char *s, int c)
-{
-	for (; *s != '\0'; s++)
-		if (*s == (const char)c)
-			return (char *)s;
-	return 0;
-}
